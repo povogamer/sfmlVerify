d1d9a60d5d0614f840e342b6b7977dc7d6c1d6fa
povogamer <76668178+povogamer@users.noreply.github.com>
2024-12-31 12:05:24 -0600

    basic verification

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200,200), "SFML Works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Blue);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
